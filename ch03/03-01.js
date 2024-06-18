6f12eb7cc3efeb700c12ac3afe995930718fb8e7
RunaUXLabs <runa.uxlabs@gmail.com>
2024-06-18 15:03:46 +0900

    렌더링 데이터 업데이트

--- ch03/03-01.js
+++ ch03/03-01.js
@@ -1,7 +1,13 @@
-var model = { message: "Hello Vue3!" };
-var vm = Vue.createApp({
+const model = {
+  message: "이해",
+  name: '루나'
+};
+// const model = { message: "Hello Vue3!" };
+let vm = Vue.createApp({
   name: "App",
   data() {
     return model;
-  },
+  },// 초기 렌더링함수가 전역으로 선언된 model객체를 퉤 뱉어내고 있음
 }).mount("#app");
+// 02-25.js에서 프록시 객체랑 동일시 하면 된다.
+// Vue.createApp({옵션})로 생성된 인스탄스객체는 반응성을 제공한다. 여기서 반응성이란 뺑글뺑글, UI뒤에 소스 일하는 순환과정을 대입하면 된다.
