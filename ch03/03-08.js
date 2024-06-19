b6f4ee0f3c746a5b067171a812dd03c03bbe94cf
RunaUXLabs <runa.uxlabs@gmail.com>
2024-06-19 13:56:59 +0900

    주석추가

--- ch03/03-08.js
+++ ch03/03-08.js
@@ -9,6 +9,7 @@ var vm = Vue.createApp({
   methods: {
     changeName(e) {
       this.name2 = e.target.value;
+      // 이벤트 함수를 만들어서 해당 이벤트가 일어나는 요소에서 value를추출해서 this(부모객체)의 name2 키의 값으로 할당하게 만든다.
     },
   },
 }).mount("#app");
