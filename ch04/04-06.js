bb59adb37cf357b8934011ea522321e87836014b
RunaUXLabs <runa.uxlabs@gmail.com>
2024-06-20 11:59:23 +0900

    주석추가

--- ch04/04-06.js
+++ ch04/04-06.js
@@ -4,6 +4,8 @@ var vm = Vue.createApp({
     return { x: 0, y: 0, sum: 0 };
   },
   watch: {
+    // 감지는 타이핑1회마다 감지함
+    // data또는속성명(변경후, 변경전){} 구조로 구성한다
     x(current, old) {
       console.log(`## x : ${old} --> ${current}`);
       var result = Number(current) + Number(this.y);
