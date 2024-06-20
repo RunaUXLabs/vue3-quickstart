39a49f64dc1d6b11a97161d8d5480d2ccefab842
RunaUXLabs <runa.uxlabs@gmail.com>
2024-06-20 11:59:06 +0900

    오타수정

--- ch04/04-01.js
+++ ch04/04-01.js
@@ -4,6 +4,6 @@ var vm = Vue.createApp({
     return { name: "" };
     // 무조건 data는 함수형이어야하며, return값으로 데이터를 적어주어야 한다. 3.x이후부터는 함수가 아니면 오류난다.
   },
-  //date: '안녕' // 에러남
-  //data: { name: "" } // 에러남
+  // data: '안녕' // 에러남
+  // data: { name: "" } // 에러남
 }).mount("#app");
