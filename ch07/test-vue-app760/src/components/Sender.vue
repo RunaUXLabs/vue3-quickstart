96a5608cc15faf981e6d5df1f32e6042f272c349
RunaUXLabs <runa.uxlabs@gmail.com>
2024-06-25 14:33:17 +0900

    에러수정 및 스타일 분리

--- ch07/test-vue-app760/src/components/Sender.vue
+++ ch07/test-vue-app760/src/components/Sender.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="box">
     <h2>Sender</h2>
     <hr />
     <button @click="sendMessage">이벤트 발신</button>
@@ -12,14 +12,15 @@ export default {
   name: "Sender",
   methods: {
     sendMessage() {
-      // this.emitter.emit("message", `${Date.now()}에 발신된 메시지`); // 밀리초로 표현됨
+      // this.emitter.emit("message", `${Date.now()}에 발신된 메시지`); // 밀초리로 표현됨
       // 중앙관리소 emitter에 메시지를 보낸다
 
       let timeStyle = {
         dateStyle: 'short',
         timeStyle: 'medium'
       };
-      let now = new Date.now();
+      let now = new Date();
+
       this.emitter.emit("message",
         `${new Intl.DateTimeFormat('kr', timeStyle).format(now)}에 발신된 메시지`
       ); // 인터내셔널 구문으로 시간정보 한국화
@@ -29,9 +30,9 @@ export default {
 </script>
 
 <style scoped>
-div {
+.box {
   border: solid 1px gray;
-  margin: 5px;
   padding: 5px;
+  box-sizing: border-box;
 }
 </style>
