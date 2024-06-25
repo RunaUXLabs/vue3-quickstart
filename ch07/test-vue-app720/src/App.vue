40756f04363a6217eece47c45ed7fc344bba1020
RunaUXLabs <runa.uxlabs@gmail.com>
2024-06-25 10:54:53 +0900

    컴포지션API에 대한 설명과 태그 작성법 추가

--- ch07/test-vue-app720/src/App.vue
+++ ch07/test-vue-app720/src/App.vue
@@ -3,7 +3,8 @@
     <h2> App 컴포넌트</h2>
     <hr />
     <ul>
-      <CheckboxItem />
+      <CheckboxItem></CheckboxItem>
+      <!-- 태그의 기본형태로도 쓸 수 있고, 단일 태그로 바꿀 수도 있다. -->
       <CheckboxItem />
       <CheckboxItem />
       <CheckboxItem />
@@ -11,18 +12,29 @@
   </div>
 </template>
 
-<!-- <script setup>
-import CheckboxItem from './components/CheckboxItem.vue';
-</script> -->
+<script setup>
+// import CheckboxItem from './components/CheckboxItem.vue';
+// 분리한 컴포넌트를 <script setup>에 넣으면 <script>에서 export default {}안에 components: {} 선언 필요없음
+
+// <script setup>에선 export default를 쓸수 없다. App.vue는 모이는 곳. 데이터 렌더링을 안하면 됨!
+// export default {
+//   name: "App",
+//   components: {
+//     CheckboxItem
+//   },
+// };
+</script>
+
 <script>
-// 분리한 컴포넌트를 <script setup>에 넣으면 export default {}에서 components: {} 선언 필요없음
 // 컴포넌트 분리가 시작되면, 경로, 파일누락, 오타에 의해 오류가 가장많이 뜬다. 화면전체가 시뻘개짐. 에러메시지를 읽고 해결하려고 노력해보자
 import CheckboxItem from './components/CheckboxItem.vue';
+// <script>안에서 import를 해도 components: {} 선언 필요없게끔 업데이트 됨!
 export default {
   name: "App",
-  components: {
-    CheckboxItem
-  },
+  // components: {
+  //   CheckboxItem
+  // },
+  // 컴포넌트 선언 생략가능해짐
 };
 </script>
 
