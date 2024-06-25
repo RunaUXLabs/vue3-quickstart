0065b239b6c8f0e785cfd26915d8120ccfdd801b
RunaUXLabs <runa.uxlabs@gmail.com>
2024-06-25 10:55:19 +0900

    전역컴포넌트 설정하는 법 추가

--- ch07/test-vue-app720/src/main.js
+++ ch07/test-vue-app720/src/main.js
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import './assets/main.css'
+import './assets/main.css';
 
-createApp(App).mount('#app')
+// 전역컴포넌트로 사용한 객체 가져오기
+// import CheckboxItem from './components/CheckboxItem.vue';
+
+createApp(App).mount('#app');
+// createApp(App)
+//   .component('CheckboxItem', CheckboxItem)
+//   // 전역컴포넌트 설정법
+//   // .component('내가쓸태그명', 컴포넌트객체)
+//   .mount('#app');
