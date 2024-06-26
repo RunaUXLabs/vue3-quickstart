648269010a88efb11310b209396aaf3ff09aec84
RunaUXLabs <runa.uxlabs@gmail.com>
2024-06-26 15:45:22 +0900

    가독성 보완위해 마크업 부분 및 주석 변경

--- ch08/v-model-test84/src/App.vue
+++ ch08/v-model-test84/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-    <label>기본 input</label>
+    <label>아무기능 안되는 껍데기 input</label>
     <InputName />
 
+    <label>:name="searchName" @update:name="changeSearchName"사용</label>
+    <InputName :name="searchName" @update:name="changeSearchName" />
     <!-- 부모에 있는 데이터를 자식에게 전달하는 방법으로 양방향 바인딩 v-model 사용가능
       자식컴포넌트에 @update:수신하는props명="값을 재할당하는 함수명" 또는
       v-model:수신하는props명="재할당되는속성명"을 적는다.
       v-model의 한글처리문제가 있어 이벤트수신처리를 했던(참고p.70)부분도 덤으로 해결가능 -->
-    <label>:name="searchName" @update:name="changeSearchName"</label>
-    <InputName :name="searchName" @update:name="changeSearchName" />
 
-    <label>v-model:name="searchName"</label>
+    <label>v-model:name="searchName"사용</label>
     <InputName v-model:name="searchName" />
-    <!-- <InputName v-model:name="searchName" v-model:email="email" /> -->
-    <!-- 여러개 v-model설정가능 -->
+    <InputName v-model:name="searchName" v-model:email="email" />
+    <!-- 여러개 v-model 설정가능하나, 기능 구현이 안되면 경고가 뜨고 구현된 기능만 반영된다. -->
 
     <h3>검색어 : {{ searchName }}</h3>
     <p>자식쪽에서 데이터 보내고, 부모쪽에서 수신됨<br>
