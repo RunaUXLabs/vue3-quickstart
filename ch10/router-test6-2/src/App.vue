9556a2b8036074bf3835eb4427b606373a206ecb
RunaUXLabs <runa.uxlabs@gmail.com>
2024-07-01 13:39:51 +0900

    <script setup>추가 및 포맷팅 변경

--- ch10/router-test6-2/src/App.vue
+++ ch10/router-test6-2/src/App.vue
@@ -31,14 +31,18 @@
   </div>
 </template>
 
-<script>
+<!-- <script>
   import Header from '@/components/Header.vue';
 
   export default {
     name: "App",
     components: { Header }
   };
+</script> -->
+<script setup>
+  import Header from '@/components/Header.vue';
 </script>
+
 <style>
   @import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
   .container {
