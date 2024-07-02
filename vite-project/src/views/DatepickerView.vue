d0c38f8ab3639e0a3322b3b1bc9486bd455d299d
RunaUXLabs <runa.uxlabs@gmail.com>
2024-07-02 13:28:56 +0900

    vuetify 아이콘 변경

--- vite-project/src/views/DatepickerView.vue
+++ vite-project/src/views/DatepickerView.vue
@@ -1,31 +1,25 @@
 <script setup>
-import { ref, computed } from 'vue';
+  import { ref, computed } from 'vue';
 
-const selectedDate = ref(null);
-const formattedDate = computed(() => {
-  if (!selectedDate.value) return null;
-  const year = selectedDate.value.getFullYear();
-  const month = (selectedDate.value.getMonth() + 1).toString().padStart(2, '0');
-  const day = selectedDate.value.getDate().toString().padStart(2, '0');
-  return `${year}-${month}-${day}`;
-});
+  const selectedDate = ref(null);
+  const formattedDate = computed(() => {
+    if (!selectedDate.value) return null;
+    const year = selectedDate.value.getFullYear();
+    const month = (selectedDate.value.getMonth() + 1).toString().padStart(2, '0');
+    const day = selectedDate.value.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  });
 </script>
 
 <template>
   <main>
     <RouterLink to="/">
-      <v-btn
-        class="ma-2"
-        color="blue-grey-darken-4">
-        <v-icon
-          icon="mdi-arrow-left"
-          start />
+      <v-btn class="ma-2" color="blue-grey-darken-4">
+        <v-icon icon="mdi-robot-love" start />
         HOME
       </v-btn>
     </RouterLink>
-    <v-date-picker
-      v-model="selectedDate"
-      show-adjacent-months />
+    <v-date-picker v-model="selectedDate" show-adjacent-months />
     <v-text-field v-model="formattedDate" label="Selected Date" readonly />
   </main>
 </template>
