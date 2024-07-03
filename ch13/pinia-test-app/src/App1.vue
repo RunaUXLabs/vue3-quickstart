cd4e8cb31790bb269251cd311399686a9e8b284c
RunaUXLabs <runa.uxlabs@gmail.com>
2024-07-03 10:10:43 +0900

    optionAPI 사용을 위한 플로우 주석 추가

--- ch13/pinia-test-app/src/App1.vue
+++ ch13/pinia-test-app/src/App1.vue
@@ -20,22 +20,29 @@
 </template>
 
 <script>
-import { useTodoList1Store } from '@/stores/todoList.js';
-import { mapState, mapActions } from 'pinia';
+  // 1.todoList.js에서 내보낸 useTodoList1Store를 가져온다.
+  import { useTodoList1Store } from '@/stores/todoList.js';
+  import { mapState, mapActions } from 'pinia';
 
-export default {
-  name: "App1",
-  data: () => ({ todo: "" }),
-  computed: {
-    // 옵션API사용시 전개구문을 자주 사용하니 체크해두기
-    ...mapState(useTodoList1Store, ['todoList', 'doneCount'])
-  },
-  methods: {
-    ...mapActions(useTodoList1Store, ['addTodo', 'deleteTodo', 'toggleDone']),
-    addTodoHandler() {
-      this.addTodo(this.todo);
-      this.todo = "";
+  // 2. export default {}의 각 키에 목적에 맞게끔 할당한다
+  // computed:{}, methods: {} 등등이 있으니 각각의 키가 무엇을 담당하는지 알고 분배 할 것
+  export default {
+    name: "App1",
+    data: () => ({ todo: "" }),
+    computed: {
+      // 3-1. useTodoList1Store에서 getters가 내보내는것들을 computed에 가져온다.
+      // 옵션API사용시 전개구문을 자주 사용하니 체크해두기
+      // ...mapState(스토어이름(변수), [꺼내올애들1,꺼내올애들2...]
+      ...mapState(useTodoList1Store, ['todoList', 'doneCount'])
+    },
+    methods: {
+      // 3-2. useTodoList1Store에서 actions가 내보내는것들을 methods에 이벤트처리 할 목적으로 가져온다.
+      // ...mapActions(스토어이름(변수), [꺼내올애들1,꺼내올애들2...]
+      ...mapActions(useTodoList1Store, ['addTodo', 'deleteTodo', 'toggleDone']),
+      addTodoHandler() {
+        this.addTodo(this.todo);
+        this.todo = "";
+      }
     }
-  }
-};
+  };
 </script>
